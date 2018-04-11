e01a3ef956889b90291ad00043a8a54b70fbb819
Julian Hamilton <Ham@Julians-MacBook-Pro.local>
2018-04-10 21:19:01 -0700

    working news fetch

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,7 +2,7 @@
 
 var url = 'https://newsapi.org/v2/everything?' +
 'q=Bitcoin&' +
-'from='+moment().format('L');+'&' +
+'from='+moment().format('L')+'&' +
 'sortBy=popularity&' +
 'apiKey=4a933268eadd4054ad084e87571eedac';
 
@@ -12,5 +12,7 @@ fetch(req)
 .then(function(response) {
 console.log(response.url);
 console.log(moment());
+$("#newdiv").text(response);
+
 });
 
