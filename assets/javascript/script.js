3db2a8370140cc6a97ff68496216cb7054c2a805
Julian Hamilton <Ham@Julians-MacBook-Pro.local>
2018-04-10 21:04:06 -0700

    update

---
+++ assets/javascript/script.js
@@ -0,0 +1,16 @@
+
+
+var url = 'https://newsapi.org/v2/everything?' +
+'q=Bitcoin&' +
+'from='+moment().format('L');+'&' +
+'sortBy=popularity&' +
+'apiKey=4a933268eadd4054ad084e87571eedac';
+
+var req = new Request(url);
+
+fetch(req)
+.then(function(response) {
+console.log(response.url);
+console.log(moment());
+});
+
