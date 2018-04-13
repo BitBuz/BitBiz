8517d7ad79c818b36814593b0f3d31ae7a27ce7c
Colin S <csmail1290@gmail.com>
2018-04-12 18:28:36 -0700

    woweewoah

--- assets/javascript/script_coindesk.js
+++ assets/javascript/script_coindesk.js
@@ -1,18 +1,4 @@
 
-
-var url = 'https://newsapi.org/v2/everything?' +
-'q=Bitcoin&' +
-'from='+moment().format('L')+'&' +
-'sortBy=popularity&' +
-'apiKey=4a933268eadd4054ad084e87571eedac';
-
-var req = new Request(url);
-
-fetch(req)
-.then(function(response) {
-console.log(response.url);
-console.log(moment());
-});
 var coinURL = "https://api.coindesk.com/v1/bpi/currentprice.json"
 var req = new Request(coinURL);
 
